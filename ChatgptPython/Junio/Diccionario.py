1d9a272164210a4d2f7254f0cd64c55e6628380f
Yuleidys Sanchez <yuleidissanchez123@gmail.com>
2026-06-12 08:54:29 -0500

    Se creo una carpeta con el nombre de W3shool, donde se añadió archivos de ramdon, replace, strip, y otra carpeta con el nombre junio donde están los archivos de Diccionario y Diccionarios Anidados

---
+++ ChatgptPython/Junio/Diccionario.py
@@ -0,0 +1,120 @@
+"""
+✅Ejercicio A
+
+Dado:
+
+persona = {
+    "nombre": "Ana",
+    "edad": 25,
+    "ciudad": "Bogotá"
+}
+
+Imprime solamente las claves usando un for.
+
+Resultado esperado:
+
+nombre
+edad
+ciudad
+
+
+persona = {
+
+    "nombre": "Ana",
+    "edad": 25,
+    "ciudad": "Bogota"
+}
+
+for clave in persona:
+
+    print(clave)"""
+
+
+
+"""✅Ejercicio B
+
+Dado:
+
+persona = {
+    "nombre": "Ana",
+    "edad": 25,
+    "ciudad": "Bogotá"
+}
+
+Imprime solamente los valores.
+
+Resultado:
+
+Ana
+25
+Bogotá
+
+persona ={
+    "nombre": "Ana",
+    "edad": 25,
+    "ciudad": "Bogota"
+}
+
+for valor in persona.values():
+    print(valor)"""
+
+
+
+"""✅Ejercicio C ⭐
+
+Dado:
+
+notas = {
+    "Matemáticas": 4.5,
+    "Programación": 5.0,
+    "Bases de Datos": 4.2
+}
+
+Calcula el promedio de las notas usando un for.
+
+Resultado:
+
+Promedio: 4.56
+
+notas = {
+
+    "Matematicas": 4.5,
+    "Programación": 5.0,
+    "Bases de Datos": 4.2
+}
+
+lista =[]
+
+for valor in notas.values():
+  
+
+  lista.append(valor)
+  entre = len(lista)
+  sumar = sum(lista)
+
+  resultado = sumar / entre
+  #sumar = sum(valor)
+print("Promedio:",resultado)
+
+
+## otra opcion de resolver el ejercicio C
+
+notas = {
+
+    "matematicas": 4.5,
+    "programacion": 5.0,
+    "base de datos": 4.2
+}
+
+suma = 0
+
+for nota in notas.values():
+
+    suma += nota
+
+    promedio = suma / len(notas)
+
+print("El promedio es:", promedio)
+"""
+
+
---
+++ ChatgptPython/Junio/DiccionariosAnidados.py
@@ -0,0 +1,90 @@
+"""
+
+Ejercicio 1
+
+Observa este diccionario:
+
+estudiante = {
+    "nombre": "Ana",
+    "edad": 22,
+    "direccion": {
+        "ciudad": "Bogotá",
+        "barrio": "Centro"
+    }
+}
+Pregunta
+
+Imprime solamente:
+
+Bogotá
+
+
+"""
+
+
+"""
+Ejercicio 2
+
+Con el mismo diccionario imprime:
+
+Centro
+
+"""
+
+
+"""
+Ejercicio 3
+
+Modifica la ciudad para que sea:
+
+Medellín
+
+Luego imprime todo el diccionario.
+
+
+"""
+
+
+
+"""
+
+Ejercicio 4 ⭐
+
+Crea este diccionario:
+
+empresa = {
+    "nombre": "TechSoft",
+    "empleado": {
+        "nombre": "Carlos",
+        "cargo": "Desarrollador",
+        "salario": 3000
+    }
+}
+
+Imprime:
+
+Carlos trabaja como Desarrollador
+
+usando los datos del diccionario.
+
+"""
+
+
+"""
+Ejercicio 5 ⭐⭐
+
+Crea un diccionario que almacene:
+
+persona = {
+    "nombre": "Yule",
+    "estudios": {
+        "carrera": "Ingeniería de Sistemas",
+        "semestre": 10
+    }
+}
+
+Y muestra:
+
+Yule estudia Ingeniería de Sistemas y está en el semestre 10
+
+"""
